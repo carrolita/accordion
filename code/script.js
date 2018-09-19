86d2d7dfe9bee47d7d37feacb883e2f80f95d1bf
Caroline Granlund <granlundcaroline@gmail.com>
2018-09-19 22:29:00 +0200

    completed assignment-caroline

--- code/script.js
+++ code/script.js
@@ -1,7 +1,9 @@
-// A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
-}
 
-// Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
+/**
+ * Toggles wether an element should be visible or not
+ *
+ * @param id {string} The id of an element
+ **/
+function toggleVisible(id) {
+  document.getElementById(id).classList.toggle('visible');
+}
